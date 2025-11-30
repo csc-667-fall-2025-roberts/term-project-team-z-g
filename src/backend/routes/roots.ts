2fb7c534dfcef0667cf4f003c4637047e5d8fde9
Geetarty <geetarth.meduri@gmail.com>
2025-11-29 20:11:12 -0800

    removed unneeded route

--- src/backend/routes/roots.ts
+++ src/backend/routes/roots.ts
@@ -7,7 +7,4 @@ router.get("/", (request, response) => {
   response.render("root", { gamesListing: ["a", "b", "c", "etc"] });
 });
 
-router.post("/", (request, response) => {
-  response.send("you posted");
-});
 export default router;
